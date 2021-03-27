34cd2653c1b8cc0192a7f7dbd626cceb741260e7
ruchika.sood <sood.ruchika@siesgst.ac.in>
2021-03-27 15:01:44 +0530

    inetgrated googlefactChk api

--- src/components/news.js
+++ src/components/news.js
@@ -1,37 +1,40 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 
 export const News = ({ isAuthenticated }) => {
-  const [news, setNews] = useState([]);
+
+  const axios = require('axios');
+  const [claims, setClaims] = useState([]);
   const [num, setNum] = useState(0);
-  const [limited, setLimited] = useState(false);
 
-  const accessKey = "ac889c73ab4e4d32b0b6b637f57e293c";
-  const date = "2021-02-27";
-  const search = "covid";
-  const url = `https://newsapi.org/v2/everything?q=${search}&from=${date}&sortBy=popularity&apiKey=${accessKey}`;
+  const accessKey = "AIzaSyBqbXZZHYtsnKpalrbMCV4dCjCYtU07Y0I";
+  const size ='100'
 
-  useEffect(() => {
-    axios
+  // const url = `https://newsapi.org/v2/everything?q=${search}&from=${date}&apiKey=${accessKey}`;
+  const url = `https://factchecktools.googleapis.com/v1alpha1/claims:search?languageCode=en&pageSize=${size}&query=covid&key=${accessKey}`
+
+
+  useEffect( async() => {
+  await  axios
       .get(url)
       .then((res) => {
-        const articles = res.data.articles;
-        setNews(articles);
-        console.log(articles);
+        const claims = res.data.claims;
+        setClaims(claims);
+        console.log("resp.data--", res.data.claims)
       })
       .catch((error) => {
         console.log(error);
-        setLimited(true);
       });
   }, []);
 
+
   return (
     <>
-      {news && num < news.length ? (
+      {claims.length ? (
         <>
           <div className="news">
-            <pre>title : {JSON.stringify(news[num].title)}</pre>
-            <pre>description : {JSON.stringify(news[num].description)}</pre>
+            <pre>title : {JSON.stringify(claims[num].text)}</pre>
+            <pre>RESULT: {JSON.stringify(claims[num]?.claimReview[0].textualRating)}</pre>
+            <pre>Description : {JSON.stringify(claims[num]?.claimReview[0].title)}</pre>
           </div>
 
           <div className="action">
@@ -41,7 +44,7 @@ export const News = ({ isAuthenticated }) => {
             </button>
           </div>
         </>
-      ) : null}
+      ) : null}  
     </>
   );
 };
