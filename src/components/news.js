f45251ee9e7b3bf084df784be0f3011d2dac25e3
jaabir <jaabir.zaid@gmail.com>
2021-03-27 20:38:58 +0530

    50% done

--- src/components/news.js
+++ src/components/news.js
@@ -1,50 +1,98 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { Button } from "@material-ui/core";
+import axios from "axios";
 
 export const News = ({ isAuthenticated }) => {
-
-  const axios = require('axios');
-  const [claims, setClaims] = useState([]);
-  const [num, setNum] = useState(0);
-
   const accessKey = "AIzaSyBqbXZZHYtsnKpalrbMCV4dCjCYtU07Y0I";
-  const size ='100'
+  const size = "100";
+  const url = `https://factchecktools.googleapis.com/v1alpha1/claims:search?languageCode=en&pageSize=${size}&query=covid&key=${accessKey}`;
 
-  // const url = `https://newsapi.org/v2/everything?q=${search}&from=${date}&apiKey=${accessKey}`;
-  const url = `https://factchecktools.googleapis.com/v1alpha1/claims:search?languageCode=en&pageSize=${size}&query=covid&key=${accessKey}`
+  const [claims, setClaims] = useState([]);
+  const random = () => Math.floor(Math.random() * (parseInt(size) - 0 + 1) + 0);
+  const [num, setNum] = useState(random());
+  const [prevNum, setPrevNum] = useState(10);
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
+  const [result, setResult] = useState(null);
 
+  const getAns = (textualRating) => {
+    const ans = textualRating.toLowerCase();
+    const isTrue = ans.includes("true");
 
-  useEffect( async() => {
-  await  axios
+    return isTrue ? "True" : "False";
+  };
+
+  const checkAnswer = (e) => {
+    if (result === null) {
+      e.target.name === answer ? setResult("correct") : setResult("wrong");
+    }
+  };
+
+  const nextQuestion = () => {
+    setResult(null);
+    setPrevNum(num);
+    let rand = random();
+
+    while (rand === prevNum) {
+      rand = random();
+    }
+
+    setNum(rand);
+    setQuestion(claims[num].text);
+  };
+
+  useEffect(async () => {
+    await axios
       .get(url)
       .then((res) => {
         const claims = res.data.claims;
         setClaims(claims);
-        console.log("resp.data--", res.data.claims)
+        setQuestion(claims[num].text);
       })
       .catch((error) => {
         console.log(error);
       });
   }, []);
 
+  useEffect(() => {
+    if (claims[num]) {
+      const ans = getAns(claims[num]?.claimReview[0].textualRating);
+      setAnswer(ans);
+    }
+  }, [question]);
 
   return (
     <>
       {claims.length ? (
         <>
           <div className="news">
-            <pre>title : {JSON.stringify(claims[num].text)}</pre>
-            <pre>RESULT: {JSON.stringify(claims[num]?.claimReview[0].textualRating)}</pre>
-            <pre>Description : {JSON.stringify(claims[num]?.claimReview[0].title)}</pre>
+            <pre>{question}</pre>
           </div>
 
           <div className="action">
-            <button onClick={() => setNum((prevNum) => prevNum + 1)}>
+            <button id="True" name="True" onClick={checkAnswer}>
+              true
+            </button>
+            <button id="False" name="False" onClick={checkAnswer}>
+              false
+            </button>
+            <Button name="next" onClick={nextQuestion}>
               {" "}
               next{" "}
-            </button>
+            </Button>
+          </div>
+
+          <div className="result">
+            {result === "correct" ? (
+              <div className="text">yes you are right</div>
+            ) : null}
+
+            {result === "wrong" ? (
+              <div className="text">nope you are wrong</div>
+            ) : null}
           </div>
         </>
-      ) : null}  
+      ) : null}
     </>
   );
 };
