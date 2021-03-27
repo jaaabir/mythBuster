93934b0050b3aa665d31b4b2c0c39b1094848e5b
k00lawn <k00lawn@gmail.com>
2021-03-27 19:31:14 +0530

    initial backend commit

---
+++ backend/models/User.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+var uniqueValidator = require('mongoose-unique-validator')
+
+const UserSchema =  mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+})
+
+
+UserSchema.plugin(uniqueValidator)
+
+const User = mongoose.model('User', UserSchema, 'users')
+
+module.exports = User;
---
+++ backend/server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const cors = require('cors')
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv')
+const logger = require('express-logger')
+const cookieParser = require('cookie-parser');
+
+const connectDB = require('./config/db')
+
+const usersRoutes = require('../nodeserver/routes/users')
+
+
+dotenv.config({ path: './config/config.env' })
+
+connectDB()
+
+const app = express();
+
+app.use(cors())
+
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/backend/nodeserver/images", express.static('images'))
+
+// app.use("/images", express.static(path.join("nodeserver/images")));
+//app.use(session({ secret: 'feeling hot hot hot' }));
+
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PATCH, DELETE, OPTIONS"
+    );
+    next();
+});
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
+app.use(logger({ path: "log/express.log" }));
+app.use(require('express-session')({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: true }));
+app.use((req, res, next) => {
+  res.locals.session = req.session;
+  next();
+});
+
+app.use("/api/user", usersRoutes)
+
+const PORT = process.env.PORT || 3000
+
+
+app.listen(PORT, () => {
+  console.log(`Server up on ${PORT}` )
+})
