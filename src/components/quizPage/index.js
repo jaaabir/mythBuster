643150da17cf3198ca4a009aea282c71f1fa121a
ruchika.sood <sood.ruchika@siesgst.ac.in>
2021-03-27 19:08:11 +0530

    added theme

--- src/components/quizPage/index.js
+++ src/components/quizPage/index.js
@@ -2,13 +2,15 @@
 import { useState, useEffect } from "react";
 import style from './style';
 import stylesheet from 'react-jss';
+import ThemeBar from "../themebar";
 
- const News = ({ isAuthenticated , classes}) => {
+ const Quiz = ({ isAuthenticated , classes}) => {
 
   const axios = require('axios');
   const [claims, setClaims] = useState([]);
   const [num, setNum] = useState(0);
-   const { base } = classes
+   const [bgColor, setBgColor] = useState('')
+   const {  container, noMargin} = classes
 
   const accessKey = "AIzaSyBqbXZZHYtsnKpalrbMCV4dCjCYtU07Y0I";
   const size ='100';
@@ -16,7 +18,10 @@ import stylesheet from 'react-jss';
   // const url = `https://newsapi.org/v2/everything?q=${search}&from=${date}&apiKey=${accessKey}`;
   const url = `https://factchecktools.googleapis.com/v1alpha1/claims:search?languageCode=en&pageSize=${size}&query=covid&key=${accessKey}`
 
-  
+   const base = {
+     height: '100vh',
+     backgroundImage: `linear-gradient(to right, ${bgColor})`,
+    }
   
   useEffect( async() => {
   await  axios
@@ -33,13 +38,16 @@ import stylesheet from 'react-jss';
 
 
   return (
-    <div className={base}>
+    <div style={base}>
+      <div style={{ position: 'relative'}}>
+        <ThemeBar selectedColor={setBgColor} />
+      </div>
       {claims.length ? (
         <div>
-          <div className="news">
-            <pre>title : {JSON.stringify(claims[num].text)}</pre>
-            <pre>RESULT: {JSON.stringify(claims[num]?.claimReview[0].textualRating)}</pre>
-            <pre>Description : {JSON.stringify(claims[num]?.claimReview[0].title)}</pre>
+          <div className={container}>
+            <p className={noMargin}>title : {JSON.stringify(claims[num].text)}</p>
+            <p className={noMargin}>RESULT: {JSON.stringify(claims[num]?.claimReview[0].textualRating)}</p>
+            <p className={noMargin}>Description : {JSON.stringify(claims[num]?.claimReview[0].title)}</p>
           </div>
 
           <div className="action">
@@ -55,4 +63,4 @@ import stylesheet from 'react-jss';
 };
 
 
-export default stylesheet(style)(News)
+export default stylesheet(style)(Quiz)
--- src/components/quizPage/style.js
+++ src/components/quizPage/style.js
@@ -4,6 +4,8 @@ export default{
 
     base : {
         height:'100vh',
-        backgroundImage: 'linear-gradient(to right, #4facfe 0%, #00f2fe 100%)' 
+        backgroundImage: 'linear-gradient(to right, #4facfe 0%, #00f2fe 100%)' ,
     }
+  
+
 }
