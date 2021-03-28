3f48e70dc2dded005b0fad15aef19695d8191618
k00lawn <k00lawn@gmail.com>
2021-03-28 13:57:41 +0530

    Update Login.js

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -50,7 +50,7 @@ const Login = (props) => {
     if (checkBtn.current.context._errors.length === 0) {
       AuthService.login(username, password).then(
         () => {
-          props.history.push("/profile");
+          props.history.push("/quiz");
           window.location.reload();
         },
         (error) => {
